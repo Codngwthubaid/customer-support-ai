666100cfa65934037850bd7d2970006c9e552a66
Codngwthubaid <codngwthubaid@gmail.com>
2025-09-03 06:17:54 +0530

    convex DB testing - successfully working on web and widget app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,20 @@
 "use client"
 
-import { useQuery } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
+import { Button } from "@workspace/ui/components/button"
+import { add_handler } from "@workspace/backend/users"
 
 export default function Page() {
 
   const user = useQuery(api.users.getMany)
+  const add_user = useMutation(api.users.add_handler)
 
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Assalam Walakium - web/app</h1>
+        <Button onClick={() => add_user()}>Add</Button>
         <p>{JSON.stringify(user)}</p>
       </div>
     </div>
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,9 +1,20 @@
-import { query } from "./_generated/server"
+import { mutation, query } from "./_generated/server"
 
 export const getMany = query({
     args: {},
     handler: async (ctx: any) => {
         const users = await ctx.db.query("users").collect();
         return users;
+    }
+})
+
+
+export const add_handler = mutation({
+    args: {},
+    handler: async (ctx: any) => {
+        const userId = await ctx.db.insert("users", {
+            name: "Ubaid"
+        })
+        return userId;
     }
 })
